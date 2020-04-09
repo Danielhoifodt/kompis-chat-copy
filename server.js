f52f02a1a52b7f4852631419556f70b0997407f0
Danielhoifodt <40982934+Danielhoifodt@users.noreply.github.com>
2020-04-09 21:56:32 +0200

    fixed bug

--- server.js
+++ server.js
@@ -81,11 +81,7 @@ io.on('connection', socket => {
 			})
 		})
 		
-	
-
-
-
-    
+	});
 
     socket.on("disconnect", () => {
 		const user = userLeave(socket.id);
@@ -99,14 +95,10 @@ io.on('connection', socket => {
 		}
 	});
 
-
-	
-    socket.on("chatMessage", (msg) => {
+    socket.on("chatMessage", msg => {
 		const user = getCurrentUser(socket.id);
         io.to(user.room).emit("message", formatMessage(user.username, msg));
-	})
-});
-	
+	})	
 })
 
 
